2a7fc7e65bbee3aa78220fcd87dc557bf4e431e8
m-shehzad455 <shehzuali5@gmail.com>
2024-05-17 22:55:36 +0500

    basic portfoio setup

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative text-red-500 flex justify-center items-center">
+      <div>
+        <h1>Hello Portfolio</h1>
+      </div>
+    </main>
+  );
+}
